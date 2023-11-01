b473f634d465c99d1adcede2ec992da2b189a094
marungsha <59702654+marungsha@users.noreply.github.com>
2023-11-01 22:12:46 +0530

    Update contract.py

--- smart_contracts/core_lw/contract.py
+++ smart_contracts/core_lw/contract.py
@@ -11,13 +11,7 @@ class IotDevice(abi.NamedTuple):
     iotURL: abi.Field[abi.String]
     ipfsHash: abi.Field[abi.String]
 
-class _owner(abi.NamedTuple):
-    addr: abi.Field[abi.Address]
-
-class iotShipping:
-    # mapping(url=> iotdevices)
-    iot_devices_mapping = BoxMapping(abi.String, IotDevice)
-
+class appState:
     # assign owner
     deployer = GlobalStateValue(
         stack_type=TealType.bytes,
@@ -25,48 +19,71 @@ class iotShipping:
         default=Global.creator_address(),
         descr="The current governor of this contract, allowed to do admin type actions",
     )
+    
+    iotDevices = BoxMapping(abi.String, IotDevice)
+    members = BoxMapping(abi.Address, abi.Uint64)
+    memberCount = GlobalStateValue(stack_type=TealType.uint64, default=Int(0), descr="Number of members registered")
 
-    owner = BoxMapping(_owner, abi.String)
-
-app = (Application("iotShipping", state= iotShipping())
+app = (Application("LW3Core1.79", state= appState())
     # On create, init app state
     .apply(unconditional_create_approval, initialize_global_state=True)
 )
 
 # Only the account set in global_state.owner may call this method
 @app.external(authorize=Authorize.only(app.state.deployer))
-def set_owner(new_governor: abi.Address) -> Expr:
-    """sets the owner of the contract, may only be called by the current governor"""
-    _addr = _owner()
-    status = abi.String()
+def setMember(newMember: abi.Address) -> Expr:
+    """sets the member of the contract, may only be called by the current governor"""
+    state = abi.Uint64()
     return Seq(
-        _addr.set(new_governor),
-        status.set('owned'),
-        app.state.owner[_addr].set(status),
+        state.set(1),
+        app.state.members[newMember].set(state),
+        app.state.memberCount.increment()
     )
 
-# function for add iot devices
+# @params `_iotURL` & `_ipfsHash`: url and the hash of the devices that user want to add
+#, replenish: abi.PaymentTransaction
+@app.external
+def addIOTDevice( _iotURL: abi.String, _ipfsHash: abi.String ) -> Expr:
+    """add your IoT Devices"""
+    iotTupple = IotDevice()
+    return Seq(
+        Assert(app.state.members[Txn.sender()].exists() == Int(1), comment="You are not authorized member"),
+        iotTupple.set(_iotURL, _ipfsHash),
+        app.state.iotDevices[_iotURL.get()].set(iotTupple),
+    )
+
 # @params `_iotURL` & `_ipfsHash`: url and the hash of the devices that user want to add
 @app.external
-def add_IOT_device(_iotURL: abi.String, _ipfsHash: abi.String) -> Expr:
-    iotShipping_tuple = IotDevice()
+def addTracePoint(_iotURL: abi.String, _ipfsHash: abi.String) -> Expr:
+    """add updated shipping details"""
+    trace = IotDevice()
     return Seq(
-        Assert(app.state.owner[Txn.sender()].exists() == Int(1)),
-        iotShipping_tuple.set(_iotURL, _ipfsHash),
-        app.state.iot_devices_mapping[_iotURL.get()].set(iotShipping_tuple),
+        Assert(app.state.members[Txn.sender()].exists() == Int(1), comment="You are not authorized member"),
+        Assert(app.state.iotDevices[_iotURL.get()].exists() == Int(1), comment="Device ID doesn't exists"),
+        trace.set(_iotURL, _ipfsHash),
+        app.state.iotDevices[_iotURL.get()].set(trace),
     )
 
-# function for get iot devices
 # @params `_iotURL`: url to get the details of the iotDevices
 @app.external(read_only=True)
-def get_IOT(_iotURL: abi.String,*,output: IotDevice)-> Expr:
+def getIOT(_iotURL: abi.String,*,output: IotDevice)-> Expr:
+    """get details about the added iot devices"""
     return Seq(
-        Assert(app.state.iot_devices_mapping[_iotURL.get()].exists() == Int(1)),
-        app.state.iot_devices_mapping[_iotURL.get()].store_into(output),
+        Assert(app.state.iotDevices[_iotURL.get()].exists() == Int(1), comment="Device ID doesn't exists"),
+        app.state.iotDevices[_iotURL.get()].store_into(output),
     )
 
-# if __name__ == "__main__":
-#     app.build().export(
-#         Path(__file__).resolve().parent / f"./artifacts/{app.name}"
-#     )
+# @params `addr`: removing address from members
+@app.external(authorize=Authorize.only(app.state.deployer))
+def deleteMember(addr: abi.Address) -> Expr:
+    """delete the member of the contract, may only be called by deployer"""
+    return Seq(
+        Pop(app.state.members[addr].delete()),
+        app.state.memberCount.decrement()
+    )
+
+if __name__ == "__main__":
+    app.build().export(
+        Path(__file__).resolve().parent / f"./artifacts/{app.name}"
+    )
 
